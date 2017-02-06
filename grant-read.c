0a29363dc408946dfc7c678899c315231eaa8a74
Chris West (Faux) <git@goeswhere.com>
2017-02-06 09:32:45 +0000

    grant-read is more of a notes file at this point

--- grant-read.c
+++ grant-read.c
@@ -10,6 +10,8 @@ void test();
 #include <sched.h>
 
 #include <sys/capability.h>
+#include <sys/stat.h>
+#include <sys/prctl.h>
 
 static int drop_setgroups() {
     FILE *fd = fopen("/proc/self/setgroups", "w");
@@ -62,17 +64,39 @@ int main(int argc, char *argv[]) {
     cap_t all = cap_get_proc();
     cap_t orig = cap_dup(all);
 
+    test();
 
-    cap_set_flag(all, CAP_INHERITABLE, 0, caps, CAP_SET);
-    cap_set_flag(all, CAP_EFFECTIVE, 0, caps, CAP_SET);
-    cap_set_flag(all, CAP_PERMITTED, 0, caps, CAP_SET);
+//    cap_set_flag(all, CAP_INHERITABLE, 1, caps, CAP_CLEAR);
+//    cap_set_flag(all, CAP_PERMITTED, 1, caps, CAP_CLEAR);
+    cap_set_flag(all, CAP_EFFECTIVE, 1, caps, CAP_SET);
     cap_set_proc(all);
 
+
+    // when we have capabilities, or change anything, /proc/sys/fs/suid_dumpable kicks in and makes us not dumpable
+    // (tested 4.8) this means that /proc/self/setgroups is chown'd to root
+    // see fs/proc/base.c's use of "inode->i_uid = GLOBAL_ROOT_UID;" where task_dumpable is false
+    // it seems that we don't require any privileges to regain this.
+
+    if (prctl(PR_SET_DUMPABLE, 1, 0, 0, 0)) {
+        perror("dumpable");
+    }
+
     test();
+
+//    execvp(argv[1], &argv[1]);
+//    perror("execvp");
+//    return 3;
+
 
     if (unshare(CLONE_NEWUSER | CLONE_NEWNS)) {
         perror("unshare");
         return 2;
+    }
+
+    test();
+
+    if (prctl(PR_SET_DUMPABLE, 1, 0, 0, 0)) {
+        perror("dumpable");
     }
 
     test();
@@ -95,18 +119,33 @@ int main(int argc, char *argv[]) {
 
     test();
 
-//    execvp(argv[1], &argv[1]);
-//    perror("execvp");
-//    return 3;
+    cap_set_proc(all);
+
+    test();
+
+    execvp(argv[1], &argv[1]);
+    perror("execvp");
+    return 3;
     return 0;
 }
 
 void test() {
     FILE *file = fopen("/etc/shadow", "r");
     if (NULL == file) {
-        perror("fopen");
+        perror("fopen shadow");
     } else {
-        printf("fopen: success\n");
+        printf("fopen shadow: success\n");
         fclose(file);
     }
+
+    printf("I am %u (%u) / %u (%u); pid: %u\n", getuid(), getgid(), geteuid(), getegid(), getpid());
+    char dat[255];
+    snprintf(dat, sizeof(dat), "/proc/%u/setgroups", getpid());
+    struct stat buf;
+    if (stat(dat, &buf)) {
+        perror("stat");
+    } else {
+        printf("file is %u (%u), inode: %lu\n", buf.st_uid, buf.st_gid, buf.st_ino);
+    }
+    printf("\n");
 }
