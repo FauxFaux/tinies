e7a0248f7152889b9d5ead0f8d69019fd4760796
Chris West (Faux) <git@goeswhere.com>
2017-02-06 14:03:58 +0000

    rewrite grant-read

--- grant-read.c
+++ grant-read.c
@@ -1,151 +1,95 @@
 #define _GNU_SOURCE
 
-void test();
-
+#include <stdbool.h>
 #include <stdio.h>
-#include <stdint.h>
+#include <string.h>
 
-#include <errno.h>
 #include <unistd.h>
-#include <sched.h>
 
 #include <sys/capability.h>
-#include <sys/stat.h>
 #include <sys/prctl.h>
 
-static int drop_setgroups() {
-    FILE *fd = fopen("/proc/self/setgroups", "w");
+#define WARN_UNUSED __attribute__((warn_unused_result))
 
-    if (NULL == fd) {
-        if (errno == ENOENT) {
-            return 0;
-        }
-        perror("couldn't deny setgroups, no file");
-        return 2;
+// @return false on success; true otherwise
+static bool WARN_UNUSED change_cap_state(const cap_value_t cap, cap_flag_t flag, cap_flag_value_t value) {
+    const cap_value_t as_array[] = {cap};
+
+    cap_t all = cap_get_proc();
+    if (!all) {
+        perror("cap_get_proc");
+        return true;
     }
 
-    if (!fputs("deny", fd)) {
-        perror("couldn't deny setgroups, write error");
-        fclose(fd);
-        return 3;
+    bool ret = true;
+
+    if (cap_set_flag(all, flag, 1, as_array, value)) {
+        perror("cap_set_flag");
+        goto done;
     }
 
-    fclose(fd);
+    if (cap_set_proc(all)) {
+        perror("cap_set_proc");
+        goto done;
+    }
 
-    return 0;
+    ret = false;
+
+    done:
+    if (cap_free(all)) {
+        perror("cap_free");
+        return true;
+    }
+
+    return ret;
 }
 
-static int map_id(const char *file, uint32_t from, uint32_t to) {
-
-    FILE *fd = fopen(file, "w");
-    if (NULL == fd) {
-        perror("opening map_id file");
-        return 4;
-    }
-
-    char buf[10 + 10 + 1 + 2 + 1];
-    sprintf(buf, "%u %u 1", from, to);
-    if (!fputs(buf, fd)) {
-        perror("couldn't write map_id file");
-        fclose(fd);
-        return 3;
-    }
-
-    fclose(fd);
-    return 0;
+static void usage(const char *argv0) {
+    fprintf(stderr, "usage: %s --chroot chroot-dir command [command-arg...]\n", argv0);
 }
-
 
 int main(int argc, char *argv[]) {
-    uid_t real_euid = geteuid();
-    gid_t real_egid = getegid();
-
-    cap_value_t caps[] = { CAP_DAC_READ_SEARCH };
-    cap_t all = cap_get_proc();
-    cap_t orig = cap_dup(all);
-
-    test();
-
-//    cap_set_flag(all, CAP_INHERITABLE, 1, caps, CAP_CLEAR);
-//    cap_set_flag(all, CAP_PERMITTED, 1, caps, CAP_CLEAR);
-    cap_set_flag(all, CAP_EFFECTIVE, 1, caps, CAP_SET);
-    cap_set_proc(all);
-
-
-    // when we have capabilities, or change anything, /proc/sys/fs/suid_dumpable kicks in and makes us not dumpable
-    // (tested 4.8) this means that /proc/self/setgroups is chown'd to root
-    // see fs/proc/base.c's use of "inode->i_uid = GLOBAL_ROOT_UID;" where task_dumpable is false
-    // it seems that we don't require any privileges to regain this.
-
-    if (prctl(PR_SET_DUMPABLE, 1, 0, 0, 0)) {
-        perror("dumpable");
+    if (argc < 4 || strcmp(argv[1], "--chroot")) {
+        usage(argv[0]);
+        return 1;
     }
 
-    test();
-
-//    execvp(argv[1], &argv[1]);
-//    perror("execvp");
-//    return 3;
-
-
-    if (unshare(CLONE_NEWUSER | CLONE_NEWNS)) {
-        perror("unshare");
-        return 2;
+    if (change_cap_state(CAP_SYS_CHROOT, CAP_EFFECTIVE, CAP_SET)) {
+        fprintf(stderr, "warning: couldn't get permission to chroot, trying anyway...\n");
     }
 
-    test();
-
-    if (prctl(PR_SET_DUMPABLE, 1, 0, 0, 0)) {
-        perror("dumpable");
+    if (chroot(argv[2])) {
+        perror("chroot");
+        return 10;
     }
 
-    test();
-
-    if (drop_setgroups()) {
-        return 3;
+    if (change_cap_state(CAP_SYS_CHROOT, CAP_EFFECTIVE, CAP_CLEAR)) {
+        fprintf(stderr, "error: couldn't remove effectiveness of cap_sys_chroot\n");
+        return 20;
     }
 
-    test();
-
-    if (map_id("/proc/self/uid_map", 0, real_euid)) {
-        return 3;
+    if (change_cap_state(CAP_SYS_CHROOT, CAP_PERMITTED, CAP_CLEAR)) {
+        fprintf(stderr, "error: couldn't un-permit cap_sys_chroot\n");
+        return 21;
     }
 
-    test();
-
-    if (map_id("/proc/self/gid_map", 0, real_egid)) {
-        return 3;
+    if (change_cap_state(CAP_DAC_READ_SEARCH, CAP_INHERITABLE, CAP_SET)) {
+        fprintf(stderr, "error: couldn't inheritable cap_dac_read_search\n");
+        return 22;
     }
 
-    test();
+    if (prctl(PR_CAP_AMBIENT, PR_CAP_AMBIENT_RAISE, CAP_DAC_READ_SEARCH, 0, 0)) {
+        perror("prctl ambient");
+        fprintf(stderr, "error: couldn't ambient cap_dac_read_search\n");
+        return 23;
+    }
 
-    cap_set_proc(all);
+    if (prctl(PR_SET_NO_NEW_PRIVS, 1, 0, 0, 0)) {
+        perror("prctl no_new_privs");
+        return 24;
+    }
 
-    test();
-
-    execvp(argv[1], &argv[1]);
+    execvp(argv[3], &argv[3]);
     perror("execvp");
     return 3;
-    return 0;
-}
-
-void test() {
-    FILE *file = fopen("/etc/shadow", "r");
-    if (NULL == file) {
-        perror("fopen shadow");
-    } else {
-        printf("fopen shadow: success\n");
-        fclose(file);
-    }
-
-    printf("I am %u (%u) / %u (%u); pid: %u\n", getuid(), getgid(), geteuid(), getegid(), getpid());
-    char dat[255];
-    snprintf(dat, sizeof(dat), "/proc/%u/setgroups", getpid());
-    struct stat buf;
-    if (stat(dat, &buf)) {
-        perror("stat");
-    } else {
-        printf("file is %u (%u), inode: %lu\n", buf.st_uid, buf.st_gid, buf.st_ino);
-    }
-    printf("\n");
 }
